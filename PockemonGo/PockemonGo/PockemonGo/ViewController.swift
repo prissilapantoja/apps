67a35ce50e162031fa96e8d5efcee99172fb4ff1
prissilapantoja <49213846+prissilapantoja@users.noreply.github.com>
2019-04-02 16:29:42 -0600

    Add files via upload
    PockemonGo

---
+++ PockemonGo/PockemonGo/PockemonGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PockemonGo
+//
+//  Created by Alumno on 4/2/19.
+//  Copyright © 2019 UTNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
